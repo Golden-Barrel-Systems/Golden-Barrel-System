5fa853cdcc544dffb8a7259169959a588ee3b9f3
Vstavo <gustavo.vsantana@sptech.school>
2026-05-22 10:13:50 -0300

    Adição do metodo das medições

--- backend/models/sensorModels.js
+++ backend/models/sensorModels.js
@@ -6,9 +6,18 @@ function buscarDadosSensor() {
         SELECT s.idSensor, s.idCamara, s.localizacao, sm.fabricante, sm.modelo, sm.numSerial, sm.dataInstalacao, sm.dataCalibracao FROM sensor s LEFT JOIN sensor_meta sm ON s.idSensor = sm.idSensor;
     `;
 
-    database.executar(instrucaoSQL);
+    return database.executar(instrucaoSQL);
+};
+
+function buscarMedicoes(idSensor) {
+    const instrucaoSQL = `
+        SELECT valor, tipo FROM medicoes WHERE idSensor = ${idSensor} ORDER BY idMedicao DESC;
+    `;
+
+    return database.executar(idSensor);
 };
 
 module.exports = {
-    buscarDadosSensor
+    buscarDadosSensor,
+    buscarMedicoes
 };
