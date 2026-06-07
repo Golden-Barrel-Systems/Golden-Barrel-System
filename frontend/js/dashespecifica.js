36f7e1b4ee7f76cbb86b21953788564498c773fc
Nicoly Baptista <nicoly.baptista@sptech.school>
2026-06-06 23:45:53 -0300

    Dash Especifica dinâmica utilizando dados do Banco

--- frontend/js/dashespecifica.js
+++ frontend/js/dashespecifica.js
@@ -1,28 +1,26 @@
 const selectCamaras = document.getElementById("selectCamaras");
 const selectSensores = document.getElementById("selectSensores");
 
-let temperaturaIdeal = 0;
-let umidadeIdeal = 0;
+let temperaturaIdeal;
+let umidadeIdeal;
 
-let tempIdealMin = temperaturaIdeal - 1; // 17
-let tempIdealMax = temperaturaIdeal + 1; // 19
+let tempIdealMin;
+let tempIdealMax;
 
-let umiIdealMin = umidadeIdeal - 1; // 64
-let umiIdealMax = umidadeIdeal + 1; // 66
+let umiIdealMin;
+let umiIdealMax;
 
-let tempAlertaMin = temperaturaIdeal - 3; // 15
-let tempAlertaMax = temperaturaIdeal + 3; // 21
+let tempAlertaMin;
+let tempAlertaMax;
 
-let umiAlertaMin = umidadeIdeal - 3; // 62
-let umiAlertaMax = umidadeIdeal + 3; // 68
+let umiAlertaMin;
+let umiAlertaMax;
 
 const GlinhaT = document.getElementById("graficLineT");
 const GlinhaU = document.getElementById("graficLineU");
 
 let graficoTemperatura = null;
 let graficoUmidade = null;
-
-// Buscando e adicionando camaras e sensores direto do banco
 
 // pegar dados do select da camara
 async function popularSelectCamara() {
@@ -65,12 +63,13 @@ async function carregarselects() {
     console.log("resposta sensor:", selectSensores.value);
 
     await coletarDados();
+
+    await buscarDadosGrafico();
   } catch (err) {
     console.log(err);
   }
 }
 carregarselects();
-carregarDados();
 
 // coletar dados para kpis
 async function coletarDados() {
@@ -105,9 +104,22 @@ async function coletarDados() {
     }
   }
 
-  temperaturaIdeal = sensoresJson[0].temperatura_ideal;
-  umidadeIdeal = sensoresJson[0].umidade_ideal;
+  temperaturaIdeal = Number(sensoresJson[0].temperatura_ideal);
+  umidadeIdeal = Number(sensoresJson[0].umidade_ideal);
 
+  tempIdealMin = temperaturaIdeal - 1;
+  tempIdealMax = temperaturaIdeal + 1;
+
+  umiIdealMin = umidadeIdeal - 1;
+  umiIdealMax = umidadeIdeal + 1;
+
+  tempAlertaMin = temperaturaIdeal - 3;
+  tempAlertaMax = temperaturaIdeal + 3;
+
+  umiAlertaMin = umidadeIdeal - 3;
+  umiAlertaMax = umidadeIdeal + 3;
+
+  // painel de resumo
   document.getElementById("resumoPainel").innerHTML =
     `${camaraNome} · ${sensorNome} - ${sensorPosicao}`;
 
@@ -115,41 +127,144 @@ async function coletarDados() {
   document.getElementById("kpiQtdSensores").innerHTML = cont;
 }
 
-function sl() {
+// função para utilizar dados dos graficos
+async function buscarDadosGrafico() {
+  const resposta = await fetch(
+    `/sensor/ultimasMedicoes/${selectSensores.value}`,
+  );
+
+  const dados = await resposta.json();
+
+  console.log(dados);
+
+  // temperatura
+  let labelsTemp = [];
+  let dadosTemp = [];
+
+  //umidade
+  let labelsUmi = [];
+  let dadosUmi = [];
+
+  // separação dados do fetch das mediçoes
+  for (let i = dados.length - 1; i >= 0; i--) {
+    let hora = dados[i].data_hora.substring(11, 16); // separar a hora do grafico em padrão ex: 11:00
+
+    if (dados[i].tipo == "temperatura") {
+      labelsTemp.push(hora);
+
+      dadosTemp.push(Number(dados[i].valor));
+    }
+
+    if (dados[i].tipo == "umidade") {
+      labelsUmi.push(hora);
+
+      dadosUmi.push(Number(dados[i].valor));
+    }
+  }
+
+  console.log("Temperatura");
+  console.log(temperaturaIdeal);
+  console.log(labelsTemp);
+  console.log(dadosTemp);
+
+  console.log("Umidade");
+  console.log(umidadeIdeal);
+  console.log(labelsUmi);
+  console.log(dadosUmi);
+
+  let ultimaTemperatura = dadosTemp[dadosTemp.length - 1];
+  let ultimaUmidade = dadosUmi[dadosUmi.length - 1];
+
+  // grafico de temperatura
+
+  if (graficoTemperatura != null) {
+    graficoTemperatura.destroy();
+  }
+
+  graficoTemperatura = new Chart(GlinhaT, {
+    type: "line",
+    data: {
+      labels: labelsTemp,
+      datasets: [
+        {
+          label: "Temperatura",
+          data: dadosTemp,
+          borderWidth: 2,
+          borderColor: "red",
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  });
+
+  // grafico de umidade
+
+  if (graficoUmidade != null) {
+    graficoUmidade.destroy();
+  }
+
+  graficoUmidade = new Chart(GlinhaU, {
+    type: "line",
+    data: {
+      labels: labelsUmi,
+      datasets: [
+        {
+          label: "Umidade",
+          data: dadosUmi,
+          borderWidth: 2,
+          borderColor: "blue",
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  });
+
   const statusTemperatura = document.getElementById("statusTemperatura");
   const statusUmidade = document.getElementById("statusUmidade");
   const statusPosicaoSensor = document.getElementById("statusPosicaoSensor");
   const statusQtdSensores = document.getElementById("statusQtdSensores");
   const statusStatusSensor = document.getElementById("statusStatusSensor");
 
+  // kpis de tem e umi
   document.getElementById("kpiTemperatura").innerHTML =
     `${ultimaTemperatura} °C`;
   document.getElementById("kpiUmidade").innerHTML = `${ultimaUmidade} %`;
 
+  document.getElementById("umidadessidea").innerHTML =
+    ` Umidade Ideal é entre ${umiIdealMin}% e ${umiIdealMax}% `;
+
+  document.getElementById("tempsidea").innerHTML =
+    ` Temperatura Ideal é entre ${tempIdealMin}°C e ${tempIdealMax}°C `;
+
+  // verificações para verificar status da camara
   if (
     ultimaTemperatura < tempAlertaMin ||
-    ultimaTemperatura > tempAlertaMax || // SENSOR 1 - temperatura critica
+    ultimaTemperatura > tempAlertaMax ||
     ultimaUmidade < umiAlertaMin ||
-    ultimaUmidade > umiAlertaMax || // SENSOR 1 - umidade critica
+    ultimaUmidade > umiAlertaMax ||
     ultimaTemperatura < tempAlertaMin ||
-    ultimaTemperatura > tempAlertaMax || // SENSOR 2 - temperatura critica
+    ultimaTemperatura > tempAlertaMax ||
     ultimaUmidade < umiAlertaMin ||
     ultimaUmidade > umiAlertaMax
   ) {
-    // SENSOR 2 - umidade critica
     document.getElementById("kpiStatusCamara").innerHTML =
       `<span style="color: red"><b>Crítico</b></span>`; // algum sensor critico = câmara critica
   } else if (
     ultimaTemperatura < tempIdealMin ||
-    ultimaTemperatura > tempIdealMax || // SENSOR 1 - temperatura alerta
+    ultimaTemperatura > tempIdealMax ||
     ultimaUmidade < umiIdealMin ||
-    ultimaUmidade > umiIdealMax || // SENSOR 1 - umidade alerta
+    ultimaUmidade > umiIdealMax ||
     ultimaTemperatura < tempIdealMin ||
-    ultimaTemperatura > tempIdealMax || // SENSOR 2 - temperatura alerta
+    ultimaTemperatura > tempIdealMax ||
     ultimaUmidade < umiIdealMin ||
     ultimaUmidade > umiIdealMax
   ) {
-    // SENSOR 2 - umidade alerta
     document.getElementById("kpiStatusCamara").innerHTML =
       `<span style="color: orange"><b>Alerta</b></span>`; // algum sensor alerta = câmara alerta
   } else {
@@ -159,6 +274,7 @@ function sl() {
   }
 
   if (
+    // vericar status de temp/umi atual
     ultimaTemperatura >= tempIdealMin &&
     ultimaTemperatura <= tempIdealMax &&
     ultimaUmidade >= umiIdealMin &&
@@ -184,221 +300,19 @@ function sl() {
     statusTemperatura.style.color = "White";
     statusUmidade.style.color = "White";
   }
-
-  if (graficoTemperatura != null) {
-    graficoTemperatura.destroy();
-  }
-
-  if (graficoUmidade != null) {
-    graficoUmidade.destroy();
-  }
-
-  graficoTemperatura = new Chart(GlinhaT, {
-    type: "line",
-    data: {
-      labels: horarios,
-      datasets: [
-        {
-          label: "Temperatura",
-          data: temperaturas,
-          borderWidth: 2,
-          borderColor: "red",
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-    },
-  });
-
-  graficoUmidade = new Chart(GlinhaU, {
-    type: "line",
-    data: {
-      labels: horarios,
-      datasets: [
-        {
-          label: "Umidade",
-          data: umidades,
-          borderWidth: 2,
-          borderColor: "blue",
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-    },
-  });
-}
-
-function carregarDados() {
-  let camaraSelecionada = Number(selectCamaras.value);
-  let sensorSelecionado = Number(selectSensores.value);
-
-  let horarios = [];
-  let temperaturas = [];
-  let umidades = [];
-
-  if (camaraSelecionada == 1 && sensorSelecionado == 1) {
-    let sensor1_horarios = [
-      "08:00",
-      "08:15",
-      "08:30",
-      "08:45",
-      "09:00",
-      "09:15",
-      "09:30",
-      "09:45",
-      "10:00",
-      "10:15",
-      "10:30",
-      "10:45",
-      "11:00",
-      "11:15",
-      "11:30",
-      "11:45",
-      "12:00",
-    ];
-
-    let sensor1_temperatura = [
-      17, 17, 17, 18, 18, 18, 18, 18, 19, 19, 19, 18, 18, 18, 18, 18, 18,
-    ];
-    let sensor1_umidade = [
-      66, 66, 65, 65, 65, 65, 64, 64, 64, 64, 64, 65, 65, 65, 65, 65, 65,
-    ];
-
-    horarios = sensor1_horarios;
-    temperaturas = sensor1_temperatura;
-    umidades = sensor1_umidade;
-
-    listaAlertas.innerHTML = `
-
-        `;
-  } else if (camaraSelecionada == 1 && sensorSelecionado == 2) {
-    let sensor2_horarios = [
-      "08:00",
-      "08:15",
-      "08:30",
-      "08:45",
-      "09:00",
-      "09:15",
-      "09:30",
-      "09:45",
-      "10:00",
-      "10:15",
-      "10:30",
-      "10:45",
-      "11:00",
-      "11:15",
-      "11:30",
-      "11:45",
-      "12:00",
-    ];
-    let sensor2_temperatura = [
-      17, 17, 18, 18, 18, 18, 18, 20, 19, 18, 18, 18, 18, 18, 18, 18, 18,
-    ];
-    let sensor2_umidade = [
-      65, 65, 65, 65, 64, 64, 64, 63, 64, 64, 65, 65, 65, 65, 65, 65, 65,
-    ];
-
-    sensor_nome = "Sensor 2";
-    camara_nome = "Câmara 1";
-    sensor_posicao = "Centro";
-
-    horarios = sensor2_horarios;
-    temperaturas = sensor2_temperatura;
-    umidades = sensor2_umidade;
-  } else if (camaraSelecionada == 2 && sensorSelecionado == 1) {
-    let sensor1_horarios = [
-      "08:00",
-      "08:15",
-      "08:30",
-      "08:45",
-      "09:00",
-      "09:15",
-      "09:30",
-      "09:45",
-      "10:00",
-      "10:15",
-      "10:30",
-      "10:45",
-      "11:00",
-      "11:15",
-      "11:30",
-      "11:45",
-      "12:00",
-    ];
-
-    let sensor1_temperatura = [
-      18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 23, 24, 24, 25, 25, 26,
-    ];
-    let sensor1_umidade = [
-      66, 65, 65, 64, 64, 63, 63, 62, 62, 61, 61, 60, 60, 59, 59, 58, 58,
-    ];
-
-    sensor_nome = "Sensor 1";
-    camara_nome = "Câmara 2";
-    sensor_posicao = "Frente";
-
-    horarios = sensor1_horarios;
-    temperaturas = sensor1_temperatura;
-    umidades = sensor1_umidade;
-  } else if (camaraSelecionada == 2 && sensorSelecionado == 2) {
-    let sensor2_horarios = [
-      "08:00",
-      "08:15",
-      "08:30",
-      "08:45",
-      "09:00",
-      "09:15",
-      "09:30",
-      "09:45",
-      "10:00",
-      "10:15",
-      "10:30",
-      "10:45",
-      "11:00",
-      "11:15",
-      "11:30",
-      "11:45",
-      "12:00",
-    ];
-    let sensor2_temperatura = [
-      18, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 23, 24, 24, 25, 25,
-    ];
-    let sensor2_umidade = [
-      65, 65, 64, 64, 63, 63, 62, 62, 61, 61, 60, 60, 59, 59, 58, 58, 57,
-    ];
-
-    sensor_nome = "Sensor 2";
-    camara_nome = "Câmara 2";
-    sensor_posicao = "Fundo";
-
-    horarios = sensor2_horarios;
-    temperaturas = sensor2_temperatura;
-    umidades = sensor2_umidade;
-  }
-
-  let ultimaTemperatura = temperaturas[temperaturas.length - 1];
-  let ultimaUmidade = umidades[umidades.length - 1];
 }
 
 selectCamaras.addEventListener("change", async function () {
   await popularSelectSensor();
   await coletarDados();
+  await buscarDadosGrafico();
 });
 
 selectSensores.addEventListener("change", async function () {
   await coletarDados();
+  await buscarDadosGrafico();
 });
 
-// função para verificação de dados
-function view() {
-  console.log(temperaturaIdeal);
-  console.log(umidadeIdeal);
-  console.log(camaraNome);
-  console.log(sensorPosicao);
-}
-
-selectCamaras.addEventListener("change", view);
+window.onload = async function () {
+  await carregarselects();
+};
