ad8d955ee47f56701e91a070e3a9921b9dd88d9f
Nicoly Baptista <nicoly.baptista@sptech.school>
2026-06-08 13:56:04 -0300

    Atualização select do listarAlertasPorSensor , adição do numero serial

--- backend/models/alertaModels.js
+++ backend/models/alertaModels.js
@@ -14,19 +14,22 @@ function buscar(idCamara) {
 
 function listarAlertasPorSensor(idSensor) {
   var instrucaoSql = `
-        SELECT
+         SELECT
             a.id_alerta,
             s.id_sensor,
             m.valor,
             m.tipo,
             a.mensagem,
             a.peso,
-            a.data_hora
+            a.data_hora,
+            sensor_meta.numero_serial AS nome 
         FROM alerta a
         JOIN medicao m
             ON a.fk_medicao = m.id_medicao
         JOIN sensor s
             ON m.fk_sensor = s.id_sensor
+            JOIN sensor_meta
+        ON sensor_meta.fk_sensor = s.id_sensor 
         WHERE s.id_sensor = ${idSensor}
         ORDER BY a.data_hora DESC
         LIMIT 10;
@@ -38,5 +41,5 @@ function listarAlertasPorSensor(idSensor) {
 module.exports = {
   registrar,
   buscar,
-  listarAlertasPorSensor
+  listarAlertasPorSensor,
 };
