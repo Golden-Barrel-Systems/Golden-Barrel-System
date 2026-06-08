7406830997f1f133aaf397b3ff8df7d94977410f
Nicoly Baptista <nicoly.baptista@sptech.school>
2026-06-07 23:31:58 -0300

    Atualização

--- backend/routes/alerta.js
+++ backend/routes/alerta.js
@@ -1,14 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const alertaController = require('../controllers/alertaController');
+const alertaController = require("../controllers/alertaController");
 
 router.post("/registrar", function (req, res) {
-    alertaController.registrar(req, res);
+  alertaController.registrar(req, res);
 });
 
 router.get("/buscar/:idCamara", function (req, res) {
-    alertaController.buscar(req, res);
+  alertaController.buscar(req, res);
+});
+
+router.get("/sensor/:idSensor", function (req, res) {
+  alertaController.listarAlertasPorSensor(req, res);
 });
 
 module.exports = router;
